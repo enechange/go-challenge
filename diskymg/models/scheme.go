3a56bb4fceb925af9f279474117eb327a8ae1feb
diskymg <52553201+diskymg@users.noreply.github.com>
2024-01-24 22:42:18 +0900

    テーブル定義

---
+++ diskymg/models/scheme.go
@@ -0,0 +1,64 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Location struct {
+	gorm.Model
+
+	ID                          string `gorm:"default:gen_random_uuid()"` // db func
+	CountryCode                 string
+	PartyID                     string
+	Publish                     bool
+	Name                        *string
+	Address                     string
+	City                        string
+	Country                     string
+	CoordinatesLatitude         string
+	CoordinatesLongitude        string
+	Evses                       []Evse
+	TimeZone                    string
+	OpeningTimesTwentyfourseven bool
+	OpeningTimesRegularHours    []LocationOpeningTimesRegularHour
+	CreatedAt                   time.Time
+	UpdatedAt                   time.Time
+}
+
+type LocationOpeningTimesRegularHour struct {
+	gorm.Model
+
+	ID          string `gorm:"default:gen_random_uuid()"` // db func
+	LocationID  string
+	Weekday     int
+	PeriodBegin string
+	PeriodEnd   string
+}
+
+type Evse struct {
+	gorm.Model
+
+	ID         string `gorm:"default:gen_random_uuid()"` // db func
+	LocationID string
+	EvseID     *string
+	Status     string
+	Connectors []Connector
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+
+type Connector struct {
+	gorm.Model
+
+	ID          string `gorm:"default:gen_random_uuid()"` // db func
+	EvseID      string
+	Standard    string
+	Format      string
+	PowerType   string
+	MaxVoltage  int
+	MaxAmperage int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
